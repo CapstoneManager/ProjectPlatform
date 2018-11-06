a6062c1bad3333e1c88964f5cb9f9fd98054c2ed
Andrew Jiang <andrewji@usc.edu>
2018-11-06 21:24:33 +0000

    correct server port

--- src/common/server.ts
+++ src/common/server.ts
@@ -1,6 +1,6 @@
 import * as jwtDecode from 'jwt-decode';
 import { IJwtToken } from './interfaces';
-export const requestEndpointBase = `//${window.location.hostname}:8433`;
+export const requestEndpointBase = `//${window.location.hostname}:8443`;
 
 export function getApiURI(endpoint: string) {
     return requestEndpointBase + endpoint;
